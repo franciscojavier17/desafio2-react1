ae95e2d5886d30b9844e4c02c1eea10edce15fdb
Francisco Moya Sanhueza <110642666+franciscojavier17@users.noreply.github.com>
2023-01-09 15:27:06 -0300

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Button from "../components/Button";
+import Input from "../components/Input";
+
+
+const Form = () => {
+    
+    const [name, setName]= useState ("");
+    const [password, setPassword] = useState("");
+    const [errorName , setErrorName]= useState ("");
+
+    const handleValidation = (e)=>{
+        e.preventDefault ();
+        setErrorName("");
+        
+        if(!name.trimStart()){
+            return setErrorName('Ingresa el Nombre')
+        }
+        if(password === "252525"){
+            return alert("Bienvenido!")
+        }
+    } 
+    
+    
+    return (
+    <>
+        <form onSubmit={handleValidation}>
+            <div className="mb-3">
+                <label  className="form-label">Nombre:</label>
+                <Input 
+                type="text"
+                onChange={(e) => setName(e.target.value)} 
+                className="form-control" 
+                placeholder="Ingresa tu Nombre"
+                value={name}
+                />
+                {
+                errorName ? <span style={{color: 'red'}}>Debes Ingresar tu Nombre</span>  : null
+                }
+            </div>
+            <div className="mb-3">
+                <label  className="form-label">Password:</label>
+                <Input 
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control" 
+                placeholder="Ingrese la Contraseña"
+                value={password}
+                />
+            </div>
+                {
+                    password === "252525"?(
+                        <Button  text="Ingresar"/>
+                    ):null
+                }
+        </form>
+    </>
+    );
+}
+export default Form;
